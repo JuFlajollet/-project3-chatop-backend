ef77a9fa7a49c91409de54adb2211ed38d2f685c
JuFlajollet <hika@me.com>
2024-04-02 01:06:43 +0200

    Fix package name

--- src/main/java/com/chatop/ChatopBackendApplication.java
+++ src/main/java/com/chatop/ChatopBackendApplication.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.ChatopBackend;
+package com.chatop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/chatop/ChatopBackendApplicationTests.java
+++ src/test/java/com/chatop/ChatopBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.openclassrooms.ChatopBackend;
+package com.chatop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
