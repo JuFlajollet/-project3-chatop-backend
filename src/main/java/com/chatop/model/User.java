90052f7903aef9fd02fc64bc808aae79d1b4a939
JuFlajollet <hika@me.com>
2024-04-04 11:38:35 +0200

    Update entities with additional properties, getter/setters and constructors

--- src/main/java/com/chatop/model/User.java
+++ src/main/java/com/chatop/model/User.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.time.LocalDate;
+
 @Entity
 public class User {
     @Id
@@ -13,6 +15,56 @@ public class User {
     private String email;
     private String name;
     private String password;
-    //TODO: add createdAt
-    //TODO: add updatedAt
+    private LocalDate createdAt;
+    private LocalDate updatedAt;
+
+    public User(String email, String name, String password, LocalDate createdAt, LocalDate updatedAt) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public static User.UserBuilder builder() {
+        return new User.UserBuilder();
+    }
+
+    public static class UserBuilder {
+
+        private String email;
+        private String name;
+        private String password;
+        private LocalDate createdAt;
+        private LocalDate updatedAt;
+
+        public User.UserBuilder setEmail(final String email) {
+            this.email = email;
+            return this;
+        }
+
+        public User.UserBuilder setName(final String name) {
+            this.name = name;
+            return this;
+        }
+
+        public User.UserBuilder setPassword(final String password) {
+            this.password = password;
+            return this;
+        }
+
+        public User.UserBuilder setCreatedAt(final LocalDate createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public User.UserBuilder setUpdatedAt(final LocalDate updatedAt) {
+            this.updatedAt = updatedAt;
+            return this;
+        }
+
+        public User build() {
+            return new User(email, name, password, createdAt, updatedAt);
+        }
+    }
 }
