1b7ef4e1469beb44613c560cd63f3905170bb12a
JuFlajollet <hika@me.com>
2024-04-09 00:47:13 +0200

    Add custom user details service to override spring security default and use our db for login

---
+++ src/main/java/com/chatop/service/CustomUserDetailsService.java
@@ -0,0 +1,44 @@
+package com.chatop.service;
+
+import com.chatop.model.DBUser;
+import com.chatop.repository.DBUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static java.lang.String.format;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private DBUserRepository DBUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<DBUser> user = DBUserRepository.findByEmail(email);
+
+        if(user.isPresent()) {
+            DBUser existingUser = user.get();
+
+            return new User(existingUser.getEmail(), existingUser.getPassword(), getGrantedAuthorities(existingUser.getRole()));
+        } else {
+            throw new UsernameNotFoundException(format("User: %s not found in db", email));
+        }
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(String role) {
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+        return authorities;
+    }
+}
