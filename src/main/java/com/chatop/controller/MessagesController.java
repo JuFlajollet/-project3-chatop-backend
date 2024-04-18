8f0cfc5543bca2f8df652b7370aafdfc3eb4b92e
JuFlajollet <hika@me.com>
2024-04-18 17:22:28 +0200

    Implement message api logic with mail sender

--- src/main/java/com/chatop/controller/MessagesController.java
+++ src/main/java/com/chatop/controller/MessagesController.java
@@ -2,6 +2,9 @@ package com.chatop.controller;
 
 import com.chatop.dto.MessageDTO;
 import com.chatop.service.MessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +19,12 @@ public class MessagesController {
     @Autowired
     private MessageService messageService;
 
-    @PostMapping(value = "/", produces = "application/json")
+    @Operation(summary = "Send a message by mail", description = "Return a success message")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Message successfully sent"),
+            @ApiResponse(responseCode = "400", description = "Bad request - There was an issue when sending the message"),
+    })
+    @PostMapping(value = "", produces = "application/json")
     public ResponseEntity<String> sendMessage(@RequestBody MessageDTO messageDTO) {
         messageService.createMessage(messageDTO);
 
--- src/main/java/com/chatop/service/MessageService.java
+++ src/main/java/com/chatop/service/MessageService.java
@@ -1,10 +1,16 @@
 package com.chatop.service;
 
+import com.chatop.dto.DBUserDTO;
 import com.chatop.dto.MessageDTO;
+import com.chatop.dto.RentalDTO;
 import com.chatop.mapper.MessageMapper;
 import com.chatop.model.Message;
 import com.chatop.repository.MessageRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,10 +20,32 @@ public class MessageService {
     private MessageRepository messageRepository;
     @Autowired
     private MessageMapper messageMapper;
+    @Autowired
+    private DBUserService dbUserService;
+    @Autowired
+    private RentalsService rentalsService;
+    @Autowired
+    private JavaMailSender emailSender;
+
+    private final static Logger logger = LoggerFactory.getLogger(MessageService.class);
 
     public void createMessage(MessageDTO newMessageDTO) {
         Message newMessage = messageMapper.toMessage(newMessageDTO);
 
         messageRepository.save(newMessage);
+        logger.info("Message successfully saved into db");
+
+        RentalDTO rental = rentalsService.getRental(newMessage.getRentalId());
+        DBUserDTO messageOwner = dbUserService.findUserById(newMessage.getUserId());
+        DBUserDTO rentalOwner = dbUserService.findUserById(rental.getOwnerId());
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("noreply@chatop.com");
+        message.setTo(rentalOwner.getEmail());
+        message.setSubject("Message about your rental: " + rental.getName());
+        message.setText(newMessageDTO.getMessage() + "\n\n" + "You can reply to your contact from Châtop at the following email address: " + messageOwner.getEmail());
+
+        emailSender.send(message);
+        logger.info("Message successfully sent");
     }
 }
